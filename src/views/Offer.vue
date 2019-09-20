ba45aa53bfd9bfca5c579750268c6de83a36f94e
lqlongqiang <1029850030@qq.com>
2019-09-20 17:19:05 +0800

    offer功能完成

--- src/views/Offer.vue
+++ src/views/Offer.vue
@@ -34,7 +34,12 @@
           <div class="tab_list">
             <el-table
               ref="multipleTable"
-              :data="OfferList"
+              :data="
+                OfferList.slice(
+                  (currentPage4 - 1) * SizeChange,
+                  currentPage4 * SizeChange
+                )
+              "
               tooltip-effect="dark"
               style="width: 100%"
               @selection-change="handleSelectionChange"
@@ -48,38 +53,110 @@
                   :prop="item.prop"
                   align="center"
                   v-if="item.display === true"
+                  show-overflow-tooltip
                 >
                 </el-table-column>
               </template>
-              <el-table-column label=""> </el-table-column>
-            </el-table>
-            <el-popover
-              placement="bottom-start"
-              width="200"
-              trigger="click"
-              class="icon"
-            >
-              <div class="Set_up">
-                <div class="Set_up_field">
-                  <span class="field_add">添加显示字段</span>
-                  <span class="field_delete">删除显示字段</span>
-                </div>
-                <div class="Type_selection">
-                  <div class="selection">选择间距类型</div>
-                  <div class="Type">
-                    <div
-                      v-for="(item, index) in Typelist"
-                      :key="item.type"
-                      :class="{ active: activeIndex === index }"
-                      @click="TypeSelection(index)"
-                    >
-                      {{ item.type }}
+              <el-table-column label="" width="140px">
+                <template slot="header">
+                  <el-popover
+                    placement="bottom-start"
+                    width="200"
+                    trigger="click"
+                    class="icon"
+                    v-model="visible"
+                  >
+                    <div class="Set_up">
+                      <div class="Set_up_field">
+                        <el-popover
+                          placement="left"
+                          width="140"
+                          trigger="click"
+                          v-model="visibles"
+                        >
+                          <div class="Add_field">
+                            <div
+                              v-for="item2 in OfferTrue"
+                              :key="item2.title"
+                              class="add"
+                            >
+                              <el-checkbox-group v-model="checkList">
+                                <el-checkbox :label="item2.title"></el-checkbox>
+                              </el-checkbox-group>
+                            </div>
+                          </div>
+                          <el-button
+                            type="primary"
+                            size="mini"
+                            style="margin-left: 80px;margin-top: 10px;"
+                            @click="Add"
+                            >确定</el-button
+                          >
+                          <span class="field_add" slot="reference"
+                            >添加显示字段</span
+                          >
+                        </el-popover>
+                        <el-popover
+                          placement="left"
+                          width="140"
+                          trigger="click"
+                          v-model="visibled"
+                        >
+                          <div class="delete_field">
+                            <div
+                              v-for="item2 in OfferFalse"
+                              :key="item2.title"
+                              class="delete"
+                            >
+                              <el-checkbox-group v-model="checkListed">
+                                <el-checkbox :label="item2.title"></el-checkbox>
+                              </el-checkbox-group>
+                            </div>
+                          </div>
+                          <el-button
+                            type="primary"
+                            size="mini"
+                            style="margin-left: 80px;margin-top: 10px;"
+                            @click="Delete"
+                            >删除</el-button
+                          >
+                          <span class="field_delete" slot="reference"
+                            >删除显示字段</span
+                          >
+                        </el-popover>
+                      </div>
+                      <div class="Type_selection">
+                        <div class="selection">选择间距类型</div>
+                        <div class="Type">
+                          <div
+                            v-for="(item, index) in Typelist"
+                            :key="item.type"
+                            :class="{ active: activeIndex === index }"
+                            @click="TypeSelection(index)"
+                          >
+                            {{ item.type }}
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <i class="el-icon-setting" slot="reference"></i>
-            </el-popover>
+                    <i class="el-icon-setting" slot="reference"></i>
+                  </el-popover>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage4"
+              :page-sizes="[10, 20, 30, 40]"
+              :page-size="10"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="OfferList.length"
+              style="margin-top: 20px;"
+            >
+            </el-pagination>
           </div>
         </el-card>
       </div>
@@ -94,8 +171,16 @@ export default {
   props: {},
   data() {
     return {
-      multipleSelection: [],
-      data: 0,
+      visible: false, //显示隐藏弹出框
+      visibles: false, //添加显示隐藏弹出框
+      visibled: false, //删除显示隐藏弹出框
+      checkList: [], //确定绑定选择字段的值
+      checkListed: [], //删除绑定选择字段的值
+      multipleSelection: [], //多选数据
+      currentPage4: 1, //分页页数
+      SizeChange: 10, //一页10条
+      OfferTrue: [], //属性为true的显示
+      OfferFalse: [], //  属性为false的显示
       ListOfHeader: [
         {
           title: "姓名",
@@ -224,6 +309,18 @@ export default {
           console.log(err);
         });
     },
+    //添加删除字段push数组
+    AddDelete() {
+      this.ListOfHeader.forEach(item => {
+        if (item.display === false) {
+          this.OfferTrue.push(item);
+          //console.log(this.OfferTrue);
+        } else {
+          this.OfferFalse.push(item);
+          //console.log(this.OfferFalse);
+        }
+      });
+    },
     //多选
     handleSelectionChange(val) {
       this.multipleSelection = val;
@@ -239,10 +336,61 @@ export default {
     //点击事件Tab切换
     TypeSelection(index) {
       this.activeIndex = index;
+    },
+    //分页内容
+    handleSizeChange(val) {
+      this.SizeChange = val;
+      console.log(`每页 ${val} 条`);
+    },
+    //分页内容
+    handleCurrentChange(val) {
+      this.currentPage4 = val;
+      console.log(`当前页: ${val}`);
+    },
+    //添加字段
+    Add() {
+      //console.log(this.checkList);
+      this.ListOfHeader.forEach(item => {
+        if (this.checkList.includes(item.title)) {
+          item.display = true;
+        }
+      });
+      this.OfferTrue.forEach(item => {
+        if (this.checkList.includes(item.title)) {
+          this.OfferFalse.push(item);
+        }
+      });
+      this.OfferTrue = this.OfferTrue.filter(item => {
+        return !this.checkList.includes(item.title);
+      });
+      this.visible = false;
+      this.visibled = false;
+      this.visibles = false;
+    },
+    //删除字段
+    Delete() {
+      //console.log(this.checkListed);
+      this.ListOfHeader.forEach(item => {
+        if (this.checkListed.includes(item.title)) {
+          item.display = false;
+        }
+      });
+      this.OfferFalse.forEach(item => {
+        if (this.checkListed.includes(item.title)) {
+          this.OfferTrue.push(item);
+        }
+      });
+      this.OfferFalse = this.OfferFalse.filter(item => {
+        return !this.checkListed.includes(item.title);
+      });
+      this.visible = false;
+      this.visibles = false;
+      this.visibled = false;
     }
   },
   mounted() {
     this.getOfferData();
+    this.AddDelete();
   },
   created() {},
   filters: {},
@@ -280,7 +428,7 @@ export default {
 }
 .icon {
   position: absolute;
-  top: 0;
+  top: -10px;
   right: 40px;
   color: #409eff;
 }
@@ -315,5 +463,11 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 0 20px;
+}
+.add {
+  padding: 2px;
+}
+.delete {
+  padding: 2px;
 }
 </style>
